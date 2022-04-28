86f1190e5e0215ae289509ea0480e75bbd2cc090
leodrumers <leodrumers@gmail.com>
2022-04-28 08:55:19 -0500

    Adding styles for buttons

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -5,5 +5,11 @@ class AppTheme {
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       primaryColor: primaryColor,
-      appBarTheme: const AppBarTheme(color: primaryColor, elevation: 0));
+      appBarTheme: const AppBarTheme(
+        color: primaryColor,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(primary: primaryColor)));
 }

25362162b0515f9aa447215fab65b4fa5607fcca
leodrumers <leodrumers@gmail.com>
2022-04-28 08:54:36 -0500

    CustomCard created

---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../themes/app_theme.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('My title'),
+            leading: Icon(
+              Icons.book_online_outlined,
+              color: AppTheme.primaryColor,
+            ),
+            subtitle: Text(
+                'Magna ex do Lorem aliqua veniam amet minim. Veniam ex dolore commodo commodo mollit officia qui. Lorem Lorem ut enim tempor quis quis nostrud ea amet nulla dolore.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Cancel')),
+              TextButton(onPressed: () {}, child: const Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

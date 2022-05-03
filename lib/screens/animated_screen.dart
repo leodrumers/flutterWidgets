2cffc535a70bc2dd292b3e1dad59d764a62a1abd
leodrumers <leodrumers@gmail.com>
2022-05-03 09:26:12 -0500

    Creating random values for container properties

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -1,13 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   const AnimatedScreen({Key? key}) : super(key: key);
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.amberAccent;
+  BorderRadiusGeometry _borderRadiusGeometry = BorderRadius.circular(10);
+
+  void changeShape() {
+    final random = Random();
+    _width = random.nextInt(300).toDouble() + 50;
+    _height = random.nextInt(300).toDouble() + 50;
+    _color = Color.fromARGB(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1,
+    );
+    _borderRadiusGeometry =
+        BorderRadius.circular(random.nextInt(100).toDouble() + 10);
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Screen'),
+      ),
       body: Center(
-        child: Text('AnimatedScreen'),
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadiusGeometry,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => changeShape(),
+        child: const Icon(
+          Icons.play_arrow_rounded,
+          size: 50,
+        ),
       ),
     );
   }

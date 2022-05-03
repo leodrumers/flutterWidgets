3057a0a41966619f0f37e61e56e4752a25f4057c
leodrumers <leodrumers@gmail.com>
2022-05-03 08:57:08 -0500

    Creating IOS and Android dialogs

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
@@ -5,10 +8,74 @@ class AlertScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+          onPressed: () => Platform.isIOS
+              ? displayIOsDialog(context)
+              : displayAndroidDialog(context),
+          child: const Text(
+            'Show Alert',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.close_outlined),
+      ),
+    );
+  }
+
+  void displayIOsDialog(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Alert IOs title'),
+            content: alertContent(),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
+  void displayAndroidDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Title'),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusDirectional.circular(20)),
+          content: alertContent(),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancelar'))
+          ],
+        );
+      },
+    );
+  }
+
+  Column alertContent() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: const [
+        Text('Alert content'),
+        SizedBox(height: 10),
+        FlutterLogo(
+          size: 100,
+        ),
+      ],
     );
   }
 }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -4,12 +4,25 @@ class AppTheme {
   static const Color primaryColor = Colors.indigoAccent;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      primaryColor: primaryColor,
-      appBarTheme: const AppBarTheme(
-        color: primaryColor,
-        elevation: 0,
-        centerTitle: true,
-      ),
-      textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(primary: primaryColor)));
+    primaryColor: primaryColor,
+    appBarTheme: const AppBarTheme(
+      color: primaryColor,
+      elevation: 0,
+      centerTitle: true,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primaryColor),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+      elevation: 5,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          primary: primaryColor.withOpacity(0.95),
+          shape: const StadiumBorder(),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          elevation: 0),
+    ),
+  );
 }

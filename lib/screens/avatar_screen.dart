7d8518000013c303ae4adb2088aaa11c8c0ec534
leodrumers <leodrumers@gmail.com>
2022-05-03 09:10:22 -0500

    Avatar screen completed

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -5,9 +5,28 @@ class AvatarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AvatarScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Avatars'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: const Text(
+                'SG',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              backgroundColor: Colors.indigo.shade700,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircleAvatar(
+          maxRadius: 100,
+          backgroundImage: NetworkImage(
+              'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80'),
+        ),
       ),
     );
   }

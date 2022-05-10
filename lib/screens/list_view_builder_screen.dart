c84187383df811754874cb00eaebbab9d94abbee
leodrumers <leodrumers@gmail.com>
2022-05-10 09:02:08 -0500

    ScrollView completed

--- lib/screens/list_view_builder_screen.dart
+++ lib/screens/list_view_builder_screen.dart
@@ -11,6 +11,7 @@ class ListViewBuilderScreen extends StatefulWidget {
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   final _scrollController = ScrollController();
   final List<int> imagesId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -20,9 +21,29 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
       double maxHeight = _scrollController.position.maxScrollExtent;
       double pixels = _scrollController.position.pixels;
       if ((pixels + 500) >= maxHeight) {
-        addImages();
+        fetchData();
       }
     });
+  }
+
+  Future fetchData() async {
+    if (_isLoading) return;
+
+    _isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    addImages();
+    _isLoading = false;
+
+    if (_scrollController.position.pixels + 100 <=
+        _scrollController.position.maxScrollExtent) return;
+
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 120,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.fastLinearToSlowEaseIn,
+    );
   }
 
   void addImages() {
@@ -31,29 +52,71 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     setState(() {});
   }
 
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final lastId = imagesId.last;
+    imagesId.clear();
+    imagesId.add(lastId + 1);
+    addImages();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppTheme.primaryColor.withOpacity(0.85),
       body: MediaQuery.removePadding(
         context: context,
         removeBottom: true,
         removeTop: true,
-        child: ListView.builder(
-          controller: _scrollController,
-          physics: const BouncingScrollPhysics(),
-          itemCount: imagesId.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-              placeholder: const AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage(
-                  'https://picsum.photos/500/300?image=${imagesId[index]}'),
-            );
-          },
+        child: Stack(
+          children: [
+            RefreshIndicator(
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                controller: _scrollController,
+                physics: const BouncingScrollPhysics(),
+                itemCount: imagesId.length,
+                itemBuilder: (context, index) {
+                  return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/500/300?image=${imagesId[index]}'),
+                  );
+                },
+              ),
+            ),
+            if (_isLoading)
+              Positioned(
+                bottom: 40,
+                left: size.width * .5 - 30,
+                child: const _LoadingIcon(),
+              ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 60,
+      width: 60,
+      decoration:
+          const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primaryColor,
       ),
     );
   }
